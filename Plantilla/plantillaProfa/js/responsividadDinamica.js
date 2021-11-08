ddf2f400105b61279c1d4686f54121501e6c41db
Renato Vargas Gómez <60189075+Renato-VG@users.noreply.github.com>
2021-11-08 15:42:19 -0600

    modulos responsivos

--- Plantilla/plantillaProfa/js/responsividadDinamica.js
+++ Plantilla/plantillaProfa/js/responsividadDinamica.js
@@ -1,22 +1,25 @@
-ajustarTamano(screen.width, screen.height);
+ajustarTamano(window.innerHeight, window.innerWidth);
 window.addEventListener('resize', function(event){
-		largoVentana=screen.height;
-		anchoVentana=screen.width;
-		
+		largoVentana=window.innerHeight;
+		anchoVentana=window.innerWidth;
 		ajustarTamano(largoVentana, anchoVentana);
 	});
 function ajustarTamano(largo, ancho){
-	if(largo>1000){
+	
+	if(ancho>1000){
+		
 		ajustarModulos(largo, ancho);
 	}
 }
 function ajustarModulos(largo, ancho){
 	var apartadosModulo = document.getElementsByClassName("apartadoModulo");
-	var anchoApartado = (55*ancho)/100;
-	alert(anchoApartado);
+	var apartadosHover = document.getElementsByClassName("apartadoHover");
+	var anchoApartado = .30*ancho; //El ancho de los apartados siempre será el 30%
 	for (i = 0; i < apartadosModulo.length; i++) {
 		apartadosModulo[i].style.width = anchoApartado+"px";
 		apartadosModulo[i].style.height = "200px";
+		apartadosHover[i].style.width = anchoApartado+"px";
+		apartadosHover[i].style.height = "200px";
 	}
 	ajustarRectangulos(largo, ancho);
 	ajustarTriangulos(largo, ancho);
@@ -26,7 +29,7 @@ function ajustarModulos(largo, ancho){
 function ajustarRectangulos(largo, ancho){
 	var i;
 	var rectangulos = document.getElementsByClassName("rectangulo");
-	var anchoRectangulos = (5*largo)/100;
+	var anchoRectangulos = .05*largo;
 	for (i = 0; i < rectangulos.length; i++) {
 		rectangulos[i].style.width = anchoRectangulos+"px";
 		rectangulos[i].style.height = "200px";
@@ -36,8 +39,8 @@ function ajustarTriangulos(largo, ancho){
 	var i;
 	var triangulos = document.getElementsByClassName("triangulo");
 	for (i = 0; i < triangulos.length; i++) {
-		triangulos[i].style.left = "95px";
-		triangulos[i].style.borderLeft = "500px solid #3CA7B8";
+		triangulos[i].style.left = "50px";
+		triangulos[i].style.borderLeft = "510px solid #3CA7B8";
 	}
 }
 function ajustarTextoModulo(largo, ancho){
