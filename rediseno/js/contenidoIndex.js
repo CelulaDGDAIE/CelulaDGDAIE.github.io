e2509154d78724c231ada640ab0dca1232d6ba3d
Renato Vargas Gómez <60189075+Renato-VG@users.noreply.github.com>
2022-03-16 14:46:18 -0600

    rediseno movil

--- rediseno/js/contenidoIndex.js
+++ rediseno/js/contenidoIndex.js
@@ -1,1 +1,9 @@
-const contenido = '{"tituloPresentacion":"Arpa Jarocha ¡Conocela!", "textoPresentacion":"<p>Holasaas</p> <p>holasus</p>", "textoUnidad":"<p>Holas</p>"}'
+const contenidoEE = '{"tituloPresentacion":"Conociendo Veracruz y sus alrededores",'+
+                    '"textoPresentacion":"<p>El estudiante expresa los movimientos naturales del cuerpo en las coordenadas de tiempo-espacio-energía, mediante la ejecución práctica, a partir de la apropiación de saberes y la autorreflexión, para desinhibirse y generar nuevas formas de manifestarse con una actitud de apertura.</p>",'+
+                    '"textoUnidad":"<p>En este primer módulo, el alumno conocerá los conceptos básicos relacionados con la expresión corporal, a partir de la observación de videos y del movimiento libre, creando un lenguaje propio, mediante el empleo del instrumento-cuerpo, sin códigos preconcebidos, siendo un modo de comunicación que encuentra su propia semántica directa, más allá de la expresión verbal conceptual.</p>",'+
+                    '"tituloModulo1":"modulo1",'+
+                    '"tituloModulo2":"modulo2",'+
+                    '"tituloModulo3":"modulo3"'+
+                '}';
+
+                
--- rediseno/js/scriptIndex.js
+++ rediseno/js/scriptIndex.js
@@ -1,13 +1,16 @@
 cargarContenido();
 window.onscroll = function() {scrollFunction()};
 function scrollFunction(){
-	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-		document.getElementById("cuadroPresentacion").style.display = "none";
-		document.getElementById("encabezadoScroll").style.display = "block";
-	  } else {
-		document.getElementById("cuadroPresentacion").style.display = "block";
-		document.getElementById("encabezadoScroll").style.display = "none";
-	  }
+	let anchoPantalla = window.innerWidth;
+	if(anchoPantalla<1000){
+		if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+			document.getElementById("cuadroPresentacion").style.display = "none";
+			document.getElementById("encabezadoScroll").style.display = "block";
+		}else{
+			document.getElementById("cuadroPresentacion").style.display = "block";
+			document.getElementById("encabezadoScroll").style.display = "none";
+		}
+	}
 }
 function abrir_menu_lateral() {
   document.getElementById("menuLateral").style.display = "block";
@@ -17,9 +20,12 @@ function cerrar_menu_lateral() {
   document.getElementById("menuLateral").style.display = "none";
 }
 function cargarContenido(){
-	const contenidoIndex = JSON.parse(contenido);
+	const contenidoIndex = JSON.parse(contenidoEE);
 	document.getElementById("tituloPresentacion").innerHTML = contenidoIndex.tituloPresentacion;
 	document.getElementById("tituloPresentacionScroll").innerHTML = contenidoIndex.tituloPresentacion;
 	document.getElementById("textoPresentacion").innerHTML = contenidoIndex.textoPresentacion;
 	document.getElementById("textoUnidad").innerHTML = contenidoIndex.textoUnidad;
+	document.getElementById("tituloModulo1").innerHTML = contenidoIndex.tituloModulo1;
+	document.getElementById("tituloModulo2").innerHTML = contenidoIndex.tituloModulo2;
+	document.getElementById("tituloModulo3").innerHTML = contenidoIndex.tituloModulo3;
 }
