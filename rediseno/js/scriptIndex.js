e74d2e685304f91c07b89dc9f9aea42ef461f3c0
Renato Vargas Gómez <60189075+Renato-VG@users.noreply.github.com>
2022-06-27 15:46:57 -0500

    rediseño

--- rediseno/js/scriptIndex.js
+++ rediseno/js/scriptIndex.js
@@ -1,5 +1,23 @@
 cargarContenido();
 window.onscroll = function() {scrollFunction()};
+let carruselInformacionDiapositivaActual = "presentacion";
+let anchoPantalla = window.innerWidth;
+if (anchoPantalla>1000){
+	let alturaPantalla = window.innerHeight;
+	let compensacion = (.10 *alturaPantalla);
+	document.getElementById("compensacionEncabezadoEscritorio").style.height = compensacion + "px";
+	let altoModulos = (.15 *alturaPantalla);
+	const modulos = document.getElementsByClassName("moduloContenido");
+	for (let i = 0; i < modulos.length; i++) {
+	  modulos[i].style.height = altoModulos + "px";
+	}
+}
+function mostrarDatos(){
+	const numerosModulos = document.getElementsByClassName("numeroModulo");
+	for (let i = 0; i < numerosModulos.length; i++) {
+	  numerosModulos[i].style.display="block";
+	}
+}
 function scrollFunction(){
 	let anchoPantalla = window.innerWidth;
 	if(anchoPantalla<1000){
@@ -15,7 +33,6 @@ function scrollFunction(){
 function abrir_menu_lateral() {
   document.getElementById("menuLateral").style.display = "block";
 }
-
 function cerrar_menu_lateral() {
   document.getElementById("menuLateral").style.display = "none";
 }
@@ -23,9 +40,44 @@ function cargarContenido(){
 	const contenidoIndex = JSON.parse(contenidoEE);
 	document.getElementById("tituloPresentacion").innerHTML = contenidoIndex.tituloPresentacion;
 	document.getElementById("tituloPresentacionScroll").innerHTML = contenidoIndex.tituloPresentacion;
+	document.getElementById("tituloEscritorio").innerHTML = contenidoIndex.tituloPresentacion;
 	document.getElementById("textoPresentacion").innerHTML = contenidoIndex.textoPresentacion;
+	document.getElementById("textoCarrusel").innerHTML = contenidoIndex.textoPresentacion;
 	document.getElementById("textoUnidad").innerHTML = contenidoIndex.textoUnidad;
 	document.getElementById("tituloModulo1").innerHTML = contenidoIndex.tituloModulo1;
 	document.getElementById("tituloModulo2").innerHTML = contenidoIndex.tituloModulo2;
 	document.getElementById("tituloModulo3").innerHTML = contenidoIndex.tituloModulo3;
+	document.getElementById("tituloModulo1Movil").innerHTML = contenidoIndex.tituloModulo1;
+	document.getElementById("tituloModulo2Movil").innerHTML = contenidoIndex.tituloModulo2;
+	document.getElementById("tituloModulo3Movil").innerHTML = contenidoIndex.tituloModulo3;
+	document.getElementById("textoCreditos").innerHTML = contenidoIndex.textoCreditos;
+}
+function cambiarDiapositiva(){
+	var aireSuperior = document.getElementById("aireSuperior");
+	var aireInferior = document.getElementById("aireInferior");
+	var cuerpoCarrusel = document.getElementById("cuerpoCarrusel");
+	const contenidoIndex = JSON.parse(contenidoEE);
+	if(carruselInformacionDiapositivaActual == "presentacion"){
+		carruselInformacionDiapositivaActual = "unidad";
+		aireSuperior.classList.remove("fondoVerde");
+		aireInferior.classList.remove("fondoVerde");
+		cuerpoCarrusel.classList.remove("fondoVerde");
+		aireSuperior.classList.add("fondoAzul");
+		aireInferior.classList.add("fondoAzul");
+		cuerpoCarrusel.classList.add("fondoAzul");
+		document.getElementById("textoCarrusel").innerHTML = contenidoIndex.textoUnidad;
+		document.getElementById("tituloCarrusel").innerHTML = "<strong><i class='bi bi-intersect'></i> Unidad de competencia</strong>";
+		document.getElementById("imagenDiapositivas").src="imagenes/unidad.jpg";
+	}else{
+		carruselInformacionDiapositivaActual = "presentacion";
+		aireSuperior.classList.remove("fondoAzul");
+		aireInferior.classList.remove("fondoAzul");
+		cuerpoCarrusel.classList.remove("fondoAzul");
+		aireSuperior.classList.add("fondoVerde");
+		aireInferior.classList.add("fondoVerde");
+		cuerpoCarrusel.classList.add("fondoVerde");
+		document.getElementById("textoCarrusel").innerHTML = contenidoIndex.textoPresentacion;
+		document.getElementById("tituloCarrusel").innerHTML = "<strong><i class='bi bi-info-circle-fill'></i> Presentacion</strong>";
+		document.getElementById("imagenDiapositivas").src="imagenes/presentacion.jpg";
+	}
 }
