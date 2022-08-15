547c41b669e07adfd2c1606d3b5869bbd6e52be8
Renato Vargas Gómez <60189075+Renato-VG@users.noreply.github.com>
2022-08-14 19:08:34 -0500

    csrdv

--- AFEL/CSRDV_rediseno/js/scriptFase3.js
+++ AFEL/CSRDV_rediseno/js/scriptFase3.js
@@ -92,3 +92,14 @@ function cambiarDiapositiva(){
 		document.getElementById("imagenDiapositivas").src="imagenes/Fase1/Proposito.jpg";
 	}
 }
+function mostrarMensajePredial(){
+	Swal.fire({
+		html: '<p style="font-size: 25px;">El <span style="color: blue;"><b>predial</b></span> es un impuesto que cobra un gobierno municipal al ciudadano due&ntilde;o de un inmueble registrado a su nombre en el Registro P&uacute;blico de la Propiedad de ese municipio. Un inmueble es una propiedad que no puede trasladarse, como un terreno, casa, departamento, edificio o f&aacute;brica. Los recursos obtenidos mediante el cobro del impuesto predial son utilizados por los gobiernos municipales para financiar obras de infraestructura que beneficien a los habitantes como calles, banquetas, alumbrado, parques p&uacute;blicos, etc&eacute;tera.</p>'
+	})
+}
+
+function mostrarMensajeTenencia(){
+	Swal.fire({
+		html: '<p style="font-size: 25px;">La <span style="color: blue;"><b>tenencia vehicular</b></span> es un tipo de impuesto directo que se cobra al ciudadano por poseer un veh&iacute;culo automotor, independiente del uso que le d&eacute;.</p> <p style="font-size: 25px;">Para obtener m&aacute;s informaci&oacute;n puedes consultar los siguientes links:</p><p><a href="https://www.bbva.mx/educacion-financiera/blog/que-es-la-tenencia-vehicular.html" target="_blank">https://www.bbva.mx/educacion-financiera/blog/que-es-la-tenencia-vehicular.html</a></p><p><a href="https://www.crabi.com/blog/tramites-vehiculares-tenencia-vehicular" target="_blank">https://www.crabi.com/blog/tramites-vehiculares-tenencia-vehicular</a></p>'
+	})
+}
